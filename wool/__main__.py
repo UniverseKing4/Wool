387837399bbd2afe4f950df5eca4c2673d37574e
JoJo <86715962+JoJoGamer2020@users.noreply.github.com>
2026-06-28 19:16:40 +0000

    Feat: Add --update flag to dynamically execute install script

--- wool/__main__.py
+++ wool/__main__.py
@@ -24,7 +24,21 @@ def main() -> None:
         action="store_true",
         help=argparse.SUPPRESS,  # hidden alias for -c
     )
+    parser.add_argument(
+        "-u",
+        "--update",
+        "--upgrade",
+        dest="update",
+        action="store_true",
+        help="Update Wool to the latest version",
+    )
     args = parser.parse_args()
+
+    if args.update:
+        import os
+        print("\033[36mℹ Updating Wool to the latest version...\033[0m")
+        os.system("curl -fsSL https://universeking4.github.io/Wool/install.sh | bash")
+        sys.exit(0)
 
     from wool.cli import run_repl
 
